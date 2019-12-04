8c29f210506f8393c7a6d8c45c48b3147b22efbf
Celeste Sippel <celestechristine@gmail.com>
2019-12-04 13:56:17 -0500

    finished

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import HelloWorld from '../components/HelloWorld'
 
 const HomePage = () => {
-  return <HelloWorld />
+  const [inches, setInches] = useState(0)
+
+  const [feet, setFeet] = useState(0)
+
+  useEffect(() => {
+    const feetTotal = (inches / 12).toFixed(2)
+    setFeet(feetTotal)
+  }, [inches])
+
+  return (
+    <>
+      <p> Type a value in the Inches field to convert the value to Feet:</p>
+      <p>
+        <label>Inches</label>
+        <input
+          id="inputInches"
+          type="number"
+          placeholder="inches"
+          onChange={e => setInches(e.target.value)}
+        ></input>
+      </p>
+      <p>
+        Feet: <span id="outputFeet">{feet}</span>
+      </p>
+    </>
+  )
 }
 
 export default HomePage
